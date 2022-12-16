b322033f974a7ab1aaed84de8d0b36f05a844db2
Bhushan  Maheshwari <bhushan.maheshwari@gmail.com>
2022-12-16 14:22:55 -0500

    updated banner section

--- src/app/modules/landing/components/banner/banner.component.ts
+++ src/app/modules/landing/components/banner/banner.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { FormControl } from '@angular/forms';
+import { Observable, of } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
 
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
   styleUrls: ['./banner.component.scss']
 })
-export class BannerComponent {
+export class BannerComponent implements OnInit {  
+  myControl = new FormControl('');
+  options: string[] = ['Indore', 'Sarafa', 'Palasia'];
+  filteredOptions: Observable<string[]> = of([]);
 
+  constructor() {
+  }
+
+  ngOnInit() {
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value || '')),
+    );
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    return this.options.filter(option => option.toLowerCase().includes(filterValue));
+  }
 }
--- src/app/modules/layout/components/header/header.component.ts
+++ src/app/modules/layout/components/header/header.component.ts
@@ -1,31 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { FormControl } from '@angular/forms';
-import firebase from 'firebase/compat/app';
-import { Observable, of } from 'rxjs';
-import { map, startWith } from 'rxjs/operators';
+import { Component, OnInit } from "@angular/core";
+import { AngularFireAuth } from "@angular/fire/compat/auth";
+import firebase from "firebase/compat/app";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.scss"],
 })
 export class HeaderComponent implements OnInit {
-  myControl = new FormControl('');
-  options: string[] = ['Indore', 'Sarafa', 'Palasia'];
-  filteredOptions: Observable<string[]> = of([]);
+  constructor(public auth: AngularFireAuth) {}
 
-  constructor(
-    public auth: AngularFireAuth
-  ) {
-  }
-
-  ngOnInit() {
-    this.filteredOptions = this.myControl.valueChanges.pipe(
-      startWith(''),
-      map(value => this._filter(value || '')),
-    );
-  }
+  ngOnInit() {}
 
   login() {
     this.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
@@ -33,11 +18,4 @@ export class HeaderComponent implements OnInit {
   logout() {
     this.auth.signOut();
   }
-
-  private _filter(value: string): string[] {
-    const filterValue = value.toLowerCase();
-    return this.options.filter(option => option.toLowerCase().includes(filterValue));
-  }
-
-
 }
